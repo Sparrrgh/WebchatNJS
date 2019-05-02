a27b071c39f9803d9603b8a387b723dcb3846b1a
Max Bellia <ma.bellia.98@gmail.com>
2019-05-02 22:06:55 +0200

    Aggiunti commenti
    Aggiunti commenti al javascript di frontend e backend

--- webapp.js
+++ webapp.js
@@ -7,36 +7,38 @@ var EventEmitter = require('events').EventEmitter
 var messageBus = new EventEmitter();
 messageBus.setMaxListeners(100)
 
-//TMP test
+//This array is to be exchanged with a DB later on
 var messages = [];
 //---------
 
-
 app.get('/room', function (req, res) {
+    //Check if it's a XMLHttpRequest
     if(req.xhr){
+        //If it is I'll add a listener to wait for a message
         var addMessageListener = function(res){
             messageBus.once('messageSent', function(data){
+                //When a message is sent I'll return it by taking it from the array
                 res.json(messages[messages.length - 1]);
             })
         }
         addMessageListener(res)
         console.log("Added one listener");
     }else{
+        //If a request is user-sent, I'll just return the chats UI
         res.sendFile(path.join(__dirname + '/public/' +'/room.html'));
         console.log('Room served');
     }
 });
 
 app.post('/room',function (req, res) {
+    //Check if it's a XMLHttpRequest
     if(req.xhr){
-        console.log(req.body)
-        
+        //I parse the message and add it to the message array
         var messageParsed =(req.body).message;
-        console.log('parsed ' + messageParsed);
         messages.push(messageParsed);
         console.log("Message sent: "+ messageParsed);
         res.send("Sent");
-        //Warns that a message as been sent
+        //Warns the listeners that a message has been sent
         messageBus.emit('messageSent');
     }
 });
