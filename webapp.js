328f9334f1ae831a96822482e3d223fb6db90bb9
Max Bellia <ma.bellia.98@gmail.com>
2019-05-03 12:43:37 +0200

    Fix esecuzione fuori dalla directory

--- webapp.js
+++ webapp.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.json())
 var path = require('path');
 var EventEmitter = require('events').EventEmitter
